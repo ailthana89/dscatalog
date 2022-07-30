51b4d63e9c6815889f8ed887bf2ddff922bc800c
ailthana89 <nathy.biblio@gmail.com>
2022-07-30 06:36:54 -0300

    Product entity, SQL Script

--- src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
-
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
