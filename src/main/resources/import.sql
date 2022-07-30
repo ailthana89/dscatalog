ed2a88bd9aa69ed310bf3f6c7436094ab347d638
ailthana89 <nathy.biblio@gmail.com>
2022-07-30 06:06:59 -0300

    Pagination

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,31 @@
-INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW())
-INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW())
-INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW())
+INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());INSERT INTO tb_category(name, created_At) VALUES ('BOOKS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('ELETRONICS', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('COMPUTERS', NOW());
